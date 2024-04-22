5acd4bfd5e520243def6a77cd5b56811a8d18f1d
Rogerwa11 <rooger.wallace@gmail.com>
2024-04-22 15:25:11 -0300

    removendo métodos não utilizaveis da classe DBConnection

--- src/main/java/com/project/seminario/DBConnection.java
+++ src/main/java/com/project/seminario/DBConnection.java
@@ -4,9 +4,6 @@
  */
 package com.project.seminario;
 
-import javax.sql.rowset.CachedRowSet;
-import javax.sql.rowset.JdbcRowSet;
-import javax.sql.rowset.RowSetProvider;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -18,20 +15,5 @@ public class DBConnection {
         String username = "root";
         String password = "root";
         return DriverManager.getConnection(url, username, password);
-    }
-
-    public static JdbcRowSet getJdbcRowSet() throws SQLException {
-        String url = "jdbc:mysql://localhost:3306/seminariocest";
-        String username = "root";
-        String password = "root";
-        JdbcRowSet jdbcRowSet = RowSetProvider.newFactory().createJdbcRowSet();
-        jdbcRowSet.setUrl(url);
-        jdbcRowSet.setUsername(username);
-        jdbcRowSet.setPassword(password);
-        return jdbcRowSet;
-    }
-
-    public static CachedRowSet getCachedRowSet() throws SQLException {
-        return RowSetProvider.newFactory().createCachedRowSet();
     }
 }
